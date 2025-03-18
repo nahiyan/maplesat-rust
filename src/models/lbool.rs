7ed55bf9da948fc29c8dcb6aad82e469d39d7769
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2025-03-18 16:08:37 -0400

    Add new traits to lbool

--- src/models/lbool.rs
+++ src/models/lbool.rs
@@ -1,7 +1,27 @@
 /// Lifted Boolean. In other words, boolean that has undefined value besides
 /// true and false.
+#[derive(Clone, PartialEq)]
 pub enum LBool {
     True = 0,
     False = 1,
     Undefined = 2,
+}
+
+impl From<bool> for LBool {
+    fn from(value: bool) -> Self {
+        match value {
+            true => LBool::True,
+            false => LBool::False,
+        }
+    }
+}
+
+impl From<i32> for LBool {
+    fn from(value: i32) -> Self {
+        match value {
+            0 => LBool::True,
+            1 => LBool::False,
+            _ => LBool::Undefined,
+        }
+    }
 }
